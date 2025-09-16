508638e13a4a8fe2f986863390e968e1ad95d5aa
Lavish Dabur <lavishdabur@gmail.com>
2025-09-16 22:57:23 +0530

    changes in bakend

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,43 +1,54 @@
+import { generatetoken } from "../utils/jwt.js";
+import prisma from "../utils/db.js"
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs";
+
 export const signup= async (req,res)=>{
-    const{fullname,email,password}=req.body
+    const { firstName, lastName, gender, dob, role, phone_no, email, password, departmentID } = req.body;
     try {
-        if(!fullname || !email || !password){
-            return res.status(400).json({message:"All fields are required"});
-        }
+        if (!firstName || !lastName || !gender || !dob || !role || !phone_no || !email || !password || !departmentID) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
         if(password.length<6){
             return res.status(400).json({message: "Password must be at least 6 characters"});
         }
 
-        const user=await User.findOne({email})
-
-        if (user) return res.status(400).json({message: "Email already exists"});
+        const existingFaculty = await prisma.faculty.findUnique({ where: { email } });
+    if (existingFaculty) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
 
         const salt=await bcrypt.genSalt(10)
         const hashedpassword=await bcrypt.hash(password,salt);
 
-        const newuser=new User({
-            fullname:fullname,
-            email:email,
-            password:hashedpassword
-        })
+        const newFaculty = await prisma.faculty.create({
+      data: {
+        FirstName: firstName,
+        LastName: lastName,
+        Gender: gender,
+        DOB: new Date(dob),
+        Role: role,
+        Phone_no: phone_no,
+        Email: email,
+        DepartmentID: parseInt(departmentID),
+        password: hashedPassword,
+      }
+    });
 
-        if(newuser){
-            await newuser.save();
-            generatetoken(newuser._id,res)
+    generatetoken(newFaculty.FacultyID, res);
 
-            res.status(201).json({
-                _id:newuser._id,
-                fullname:newuser.fullname,
-                email:newuser.email,
-                profilePic:newuser.profilePic,
-            });
-        }else{
-            res.staus(400).json({message: "Invalid user data"});
-            res.status(500).json({message:"Internal Server Error"});
-        }
 
+    res.status(201).json({
+      FacultyID: newFaculty.FacultyID,
+      FirstName: newFaculty.FirstName,
+      LastName: newFaculty.LastName,
+      Email: newFaculty.Email,
+      Role: newFaculty.Role,
+    });
+        
     } catch (error) {
         console.log("Error in signup controller ",error.message);
+        res.status(500).json({message:"Internal Server Error"});
     }
 };
 export const login=()=>{
